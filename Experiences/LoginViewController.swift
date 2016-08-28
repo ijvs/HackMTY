f781a99ea2e82444bad67f0eb67b96d48fbada5b
Abner <abner.abbey@hotmail.com>
2016-08-27 23:24:02 -0500

    user data goes to defaults

--- Experiences/LoginViewController.swift
+++ Experiences/LoginViewController.swift
@@ -37,9 +37,29 @@ class LoginViewController: UIViewController
     {
         FBSDKLoginManager().logInWithReadPermissions(["public_profile", "email", "user_friends"], fromViewController: self) { (result, error) in
             
-            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, email, name, picture"]).startWithCompletionHandler({ (connection, result, error) -> Void in
-                print(result)
-            })
+            if error == nil{
+                FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, email, name, picture"]).startWithCompletionHandler({ (connection, result, error) -> Void in
+                    if error == nil{
+                        let resultDict = result as! NSDictionary
+                        print(resultDict)
+                        let email = resultDict.objectForKey("email") as! String
+                        let id = resultDict.objectForKey("id") as! String
+                        let name = resultDict.objectForKey("name") as! String
+                        
+                        let pictureDict = resultDict.objectForKey("picture") as! NSDictionary
+                        let dataPicture = pictureDict.objectForKey("data") as! NSDictionary
+                        
+                        let urlPicture = dataPicture.objectForKey("url") as! String
+                        
+                        let defaults = NSUserDefaults.standardUserDefaults()
+                        defaults.setObject(email, forKey: "user_email")
+                        defaults.setObject(id, forKey: "user_id")
+                        defaults.setObject(name, forKey: "user_name")
+                        defaults.setObject(urlPicture, forKey: "user_urlPicture")
+                        defaults.synchronize()
+                    }
+                })
+            }
         }
     }
 
