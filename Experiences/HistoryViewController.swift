b9d14ad28b3be4cf7f166e758a9c47f1942d97ec
Abner <abner.abbey@hotmail.com>
2016-08-28 10:49:02 -0500

    It's done

--- Experiences/HistoryViewController.swift
+++ Experiences/HistoryViewController.swift
@@ -17,6 +17,8 @@ class HistoryViewController: UIViewController
         super.viewDidLoad()
 
         self.navigationItem.title = "History"
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: #selector(shareExp))
+        self.navigationItem.rightBarButtonItem = shareButton
         
         let skView = SKView(frame: self.view.bounds)
         self.view.addSubview(skView)
@@ -24,6 +26,20 @@ class HistoryViewController: UIViewController
         let scene = HistoryScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
         skView.presentScene(scene)
+    }
+    
+    func shareExp()
+    {
+        let activityView = UIActivityViewController(activityItems: [], applicationActivities: nil)
+        let excludeActivities = [UIActivityTypeAirDrop,
+                                 UIActivityTypePrint,
+                                 UIActivityTypeAssignToContact,
+                                 UIActivityTypeSaveToCameraRoll,
+                                 UIActivityTypeAddToReadingList,
+                                 UIActivityTypePostToFlickr,
+                                 UIActivityTypePostToVimeo]
+        activityView.excludedActivityTypes = excludeActivities
+        self.presentViewController(activityView, animated: true, completion: nil)
     }
     
     
