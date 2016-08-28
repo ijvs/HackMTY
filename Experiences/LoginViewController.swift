8a2a4470deeb588de9f07004b112b1de090e7eaf
Abner <abner.abbey@hotmail.com>
2016-08-27 19:41:48 -0500

    login facebook almost finished

--- Experiences/LoginViewController.swift
+++ Experiences/LoginViewController.swift
@@ -20,16 +20,27 @@ class LoginViewController: UIViewController
         super.viewDidLoad()
         
         self.loginButton.layer.cornerRadius = 7
-        self.loginButton.hidden = true
         
-        let login = FBSDKLoginButton()
+        /*let login = FBSDKLoginButton()
+        login.delegate = self
         login.readPermissions = ["public_profile", "email", "user_friends"]
         
         login.frame = CGRect(x: self.view.frame.size.width/2 - 100, y: self.view.frame.size.height/2 - 22, width: 200, height: 44)
         login.layer.cornerRadius = 7
         login.layer.masksToBounds = true
         login.center = self.view.center
-        self.view.addSubview(login)
+        self.view.addSubview(login)*/
+    }
+    
+
+    @IBAction func loginWithFacebook(sender: UIButton)
+    {
+        FBSDKLoginManager().logInWithReadPermissions(["public_profile", "email", "user_friends"], fromViewController: self) { (result, error) in
+            
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, email, name"]).startWithCompletionHandler({ (connection, result, error) -> Void in
+                print(result)
+            })
+        }
     }
 
 }
